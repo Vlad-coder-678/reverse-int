5f1d1353e6ce8e2291341f910b187be216bc74fc
Vlad-coder-678 <vova.mezencev@rambler.ru>
2021-08-14 21:58:44 +0300

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    return Number(
+        n
+            .toString()
+            .split("")
+            .map((i) => (i === "-" ? "" : i))
+            .reverse()
+            .join("")
+    );
+};
